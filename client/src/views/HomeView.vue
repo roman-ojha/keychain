a78fd47dc5a2b84d17cf4faefe9a0b7387ee2101
Roman-Ojha <razzroman98@gmail.com>
2023-01-02 22:45:43 +0545

    fix: cleaned vue project

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <main>
-        <TheWelcome />
-    </main>
+    <div>
+        <h1>Home</h1>
+    </div>
 </template>
---
+++ client/src/views/SignInView.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: "SignInView",
+});
+</script>
+
+<template>
+    <div>
+        <h1>Signin</h1>
+    </div>
+</template>
+
+<style scoped></style>
