a4d6f4f2aabd9654b8f04fa75bbbc7acc4371b48
Roman-Ojha <razzroman98@gmail.com>
2023-01-03 09:17:11 +0545

    feat: styled & added background and icon on signin page

--- client/src/views/SignInView.vue
+++ client/src/views/SignInView.vue
@@ -1,14 +1,35 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import logo from "../assets/img/appIcon.png";
 export default defineComponent({
     name: "SignInView",
+    data() {
+        return {
+            logo,
+            name: "roman",
+        };
+    },
 });
 </script>
 
 <template>
-    <div>
-        <h1>Signin</h1>
+    <div class="container">
+        <div class="background-container">
+            <div class="background-container__round-shape"></div>
+        </div>
+        <main class="main">
+            <div class="main__logo-title">
+                <img class="main__logo-title__logo" :src="logo" />
+                <h1>KeyChain</h1>
+            </div>
+            <div class="main__sign-in">
+                <h2>Sign In</h2>
+            </div>
+            <div class="main__form-container"></div>
+        </main>
     </div>
 </template>
 
-<style scoped lang="scss" src="../styles/views/signin.module.scss"></style>
+<style scoped lang="scss">
+@import "../styles/views/signin.module.scss";
+</style>
