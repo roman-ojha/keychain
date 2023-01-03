85bf26bf6f76b439a16d6ca8710b5a829bbcbde6
Roman-Ojha <razzroman98@gmail.com>
2023-01-03 17:01:31 +0545

    feat: created template & added some style for UnlockVault component

--- client/src/views/UnlockVault.vue
+++ client/src/views/UnlockVault.vue
@@ -1,7 +1,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import logo from "../assets/img/appIcon.png";
 export default defineComponent({
     name: "UnlockVault",
+    data() {
+        return {
+            logo,
+        };
+    },
 });
 </script>
 
@@ -10,6 +16,29 @@ export default defineComponent({
         <div class="background-container">
             <div class="background-container__round-shape"></div>
         </div>
+        <main class="main">
+            <div class="main__logo-title">
+                <img
+                    class="main__logo-title__logo"
+                    :src="logo"
+                    alt="app-logo"
+                />
+                <h1>KeyChain</h1>
+            </div>
+            <form action="" class="form">
+                <p class="form_desc">
+                    Enter you master password to unlock vault
+                </p>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Master Password"
+                />
+                <input type="submit" value="Unlock" class="form__unlock-btn" />
+                <button class="form__logout-btn">Log out</button>
+            </form>
+        </main>
     </div>
 </template>
 
