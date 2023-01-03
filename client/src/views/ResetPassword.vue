184160894a6aecfe779b9fbbcb447a010cc23a52
Roman-Ojha <razzroman98@gmail.com>
2023-01-03 18:21:19 +0545

    feat: create UI for ResetPassword page

---
+++ client/src/views/ResetPassword.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import logo from "../assets/img/appIcon.png";
+import FormVue from "@/components/ForgetPassword/Form.vue";
+export default defineComponent({
+    name: "UnlockVault",
+    data() {
+        return {
+            logo,
+        };
+    },
+    components: {
+        FormVue,
+    },
+});
+</script>
+
+<template>
+    <div class="container">
+        <div class="background-container">
+            <div class="background-container__round-shape"></div>
+        </div>
+        <main class="main">
+            <div class="main__logo-title">
+                <img
+                    class="main__logo-title__logo"
+                    :src="logo"
+                    alt="app-logo"
+                />
+                <h1>KeyChain</h1>
+            </div>
+            <FormVue />
+        </main>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@import "../styles/views/forgetPassword.module.scss";
+</style>
