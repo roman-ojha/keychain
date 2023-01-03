f247146a16b2fccec88a877849a1e8de1f8a2015
Roman-Ojha <razzroman98@gmail.com>
2023-01-03 16:09:35 +0545

    refactor: seperated Register page Form Componenet

--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -1,18 +1,16 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import appIcon from "../assets/img/appIcon.png";
-import "@vuepic/vue-datepicker/src/VueDatePicker/style/main.scss";
-import { Icon } from "@iconify/vue";
+import FormVue from "../components/Register/Form.vue";
 export default defineComponent({
     name: "RegisterView",
     data() {
         return {
             appIcon,
-            date: new Date(),
         };
     },
     components: {
-        Icon,
+        FormVue,
     },
 });
 </script>
@@ -31,81 +29,7 @@ export default defineComponent({
                     <img :src="appIcon" alt="appicon" />
                     <h1>KeyChain</h1>
                 </div>
-                <form class="form">
-                    <input type="text" name="name" placeholder="Full Name" />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email Address"
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                    />
-                    <input
-                        type="password"
-                        name="password_confirmation"
-                        placeholder="Confirm Password"
-                    />
-                    <div class="form__birthday">
-                        <label for="birthday">Birthday</label>
-                        <DatePicker
-                            v-model="date"
-                            id="birthday"
-                            class="form__birthday__picker"
-                            :enable-time-picker="false"
-                            :max-date="new Date()"
-                            :year-range="[
-                                new Date().getFullYear() - 100,
-                                new Date().getFullYear(),
-                            ]"
-                        ></DatePicker>
-                    </div>
-                    <div class="form__gender">
-                        <div class="form__gender__child">
-                            <label for="male">Male</label>
-                            <input
-                                type="checkbox"
-                                id="male"
-                                name="gender"
-                                value="male"
-                            />
-                        </div>
-                        <div class="form__gender__child">
-                            <label for="female">Female</label>
-                            <input
-                                type="checkbox"
-                                id="female"
-                                name="gender"
-                                value="female"
-                            />
-                        </div>
-                        <div class="form__gender__child">
-                            <label for="others">Others</label>
-                            <input
-                                type="checkbox"
-                                id="others"
-                                name="gender"
-                                value="others"
-                            />
-                        </div>
-                    </div>
-                    <div class="form__buttons">
-                        <input
-                            type="submit"
-                            value="Sign Up"
-                            class="form__buttons__sign-up"
-                        />
-                        <RouterLink to="/signin" class="form__buttons__sign-in"
-                            >Sign In
-                            <Icon
-                                icon="ic:outline-keyboard-double-arrow-right"
-                                color="black"
-                            />
-                        </RouterLink>
-                    </div>
-                </form>
+                <FormVue />
             </div>
         </main>
     </div>
